058a63da397f8b6dc09348c4d8e2dd8d4c3df6eb
Lionel LIMOL <limol.lionel@gmail.com>
2022-10-29 12:11:36 -0400

    Add wallet method

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 export const networkconfig = {
   testnet: {
+    "app.detail.title": "Fancraze ltd.",
+    "app.detail.icon":
+      "https://pbs.twimg.com/profile_images/1449458213116526592/4jVUAYLg_400x400.jpg",
     "accessNode.api": "https://rest-testnet.onflow.org",
     "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
+    "discovery.wallet.method": "IFRAME/RPC",
     "flow.network": "testnet",
     "0xFungibleToken": "0x9a0766d93b6608b7",
     "0xFUSD": "0xe223d8a629e49c68",
@@ -11,8 +15,12 @@ export const networkconfig = {
     "0xBloctoToken": "0x6e0797ac987005f5",
   },
   mainnet: {
+    "app.detail.title": "Fancraze ltd.",
+    "app.detail.icon":
+      "https://pbs.twimg.com/profile_images/1449458213116526592/4jVUAYLg_400x400.jpg",
     "accessNode.api": "https://rest-mainnet.onflow.org",
     "discovery.wallet": "https://fcl-discovery.onflow.org/authn",
+    "discovery.wallet.method": "IFRAME/POST",
     "flow.network": "mainnet",
     "0xFungibleToken": "0xf233dcee88fe0abe",
     "0xFUSD": "0x3c5959b568896393",
